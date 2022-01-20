50a68faa32ce5f3474107f3ed12cc4592f80f477
HeloiseFerla <ferla.heloise@gmail.com>
2022-01-21 00:06:33 +0100

    Fix and route post tip

--- routes/item.js
+++ routes/item.js
@@ -25,7 +25,7 @@ itemRouter.get('/:id/articles', async (req, res) => {
   try {
     const { id } = req.params;
     const [articles] = await db.query(
-      'SELECT Article.name, Article.price, Article.link FROM Articles JOIN Article ON Articles.articleId = Article.id WHERE Articles.itemId = ?',
+      'SELECT Article.name, Article.price, Article.link, Article.image FROM Articles JOIN Article ON Articles.articleId = Article.id WHERE Articles.itemId = ?',
       [id]
     );
     res.status(200).json(articles);
@@ -38,7 +38,7 @@ itemRouter.get('/:id/pictures', async (req, res) => {
   try {
     const { id } = req.params;
     const [pictures] = await db.query(
-      ' SELECT * FROM Picture WHERE itemId = ?',
+      ' SELECT Picture.picture, User.username FROM Picture JOIN User ON Picture.userId = User.id WHERE itemId = ?',
       [id]
     );
     res.status(200).json(pictures);
@@ -50,10 +50,27 @@ itemRouter.get('/:id/pictures', async (req, res) => {
 itemRouter.get('/:id/tips', async (req, res) => {
   try {
     const { id } = req.params;
-    const [tips] = await db.query(' SELECT * FROM Tip WHERE itemId = ?', [id]);
+    const [tips] = await db.query(
+      ' SELECT Tip.comment, User.username  FROM Tip JOIN User ON Tip.userId = User.id WHERE itemId = ?',
+      [id]
+    );
     res.status(200).json(tips);
   } catch (err) {
     res.status(400).send(err);
   }
 });
+
+itemRouter.post('/tips', async (req, res) => {
+  try {
+    const tip = req.body;
+    const [resSql] = await db.query(
+      ' INSERT INTO Tip (itemId, userId, comment) VALUES ?',
+      [tip]
+    );
+    res.status(200).json(resSql);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
 module.exports = itemRouter;
